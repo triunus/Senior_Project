494aec766a332d7d829692241fe3db04cb65817a
triunus <74586287+triunus@users.noreply.github.com>
2022-08-04 22:40:47 +0900

    Add Serialization Test Code

---
+++ Game_Script/SerializationAndStreamTest/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+[Serializable]
+public class AccountStruct
+{
+    private string userID;
+    private string userMACAddress;
+
+    public AccountStruct(string userID = "", string userMACAddress = "")
+    {
+        this.userID = userID;
+        this.userMACAddress = userMACAddress;
+    }
+
+    public string UserID { get; set; }
+
+    public string UserMACAddress { get; set; }
+
+}
---
+++ Game_Script/SerializationAndStreamTest/Program.cs
@@ -0,0 +1,50 @@
+﻿using System;
+
+using System.IO;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Runtime.Serialization;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        AccountStruct accountStruct = new AccountStruct();
+        IFormatter binaryFormatter_read = new BinaryFormatter();
+        IFormatter BinaryFormatter_write = new BinaryFormatter();
+
+        try
+        {
+            // Open은 운영 체제에서 기존 파일을 열도록 지정합니다.
+            // 파일을 열 수 있는지 여부는 FileAccess 열거형에서 지정된 값에 따라 달라집니다.
+            // 파일이 없으면 FileNotFoundException 예외가 throw됩니다.
+            Stream readInfo = new FileStream("./test.txt", FileMode.Open, FileAccess.Read, FileShare.Read);
+
+            accountStruct = (AccountStruct)binaryFormatter_read.Deserialize(readInfo);
+            readInfo.Close();
+
+            Console.WriteLine("UserID : " + accountStruct.UserID);
+            Console.WriteLine("UserID : " + accountStruct.UserMACAddress);
+            Console.ReadLine();
+        }
+        catch (FileNotFoundException ex01)
+        {
+            Console.WriteLine("Exception : " + ex01);
+
+            try
+            {
+                accountStruct.UserID = "Triunus";
+                accountStruct.UserMACAddress = "TriunusMACAddress";
+
+                Stream writeinfo = new FileStream("./test.txt", FileMode.Create, FileAccess.Write, FileShare.None);
+
+                BinaryFormatter_write.Serialize(writeinfo, accountStruct);
+
+                writeinfo.Close();
+            }
+            catch(UnauthorizedAccessException ex02)
+            {
+                Console.WriteLine("Exception : " + ex02);
+            }
+        }
+    }
+}
