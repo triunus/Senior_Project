63aedf80998ac84d932c4cbd96d274af5450d126
triunus <74586287+triunus@users.noreply.github.com>
2022-08-05 22:55:38 +0900

    Add Attribute Test Code

---
+++ Game_Script/Attributetest/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+[System.AttributeUsage(System.AttributeTargets.Class | System.AttributeTargets.Struct) ]
+public class AuthorAttribute : System.Attribute
+{
+    private string name;
+    public double version;
+
+    public AuthorAttribute(string name)
+    {
+        this.name = name;
+        version = 1.0;
+    }
+    public string Name() { return name; }
+}
+
+[Author("P. Ackerman", version = 1.1)]
+class Program
+{
+    static void Main(string[] args)
+    {
+        AuthorAttribute author = (AuthorAttribute)Attribute.GetCustomAttribute(typeof(Program), typeof(AuthorAttribute));
+
+        Console.WriteLine(author.Name());
+        Console.WriteLine(author.version);
+        Console.ReadLine();
+    }
+}
+
